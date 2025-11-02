ff11acae3433f20bd2f75b1d7e8f370918368580
ThanuraSilva <thanurasilva1989@gmail.com>
2025-11-02 22:06:12 +0530

    Create base structure

--- src/main/java/com/thanu/came/Demo.java
+++ src/main/java/com/thanu/came/Demo.java
@@ -1,12 +1,14 @@
 package com.thanu.came;
 
 import com.thanu.came.challange2.ArraySort;
+import com.thanu.came.challange3.DataHandler;
 
 public class Demo {
     public static void main(String[] args) {
-        print();
-        int[] myArray = {-5,20,10,55,10,0,2,1} ;
-        new ArraySort().sortArray(myArray);
+         new DataHandler().dataAdding();
+//        print();
+//        int[] myArray = {-5,20,10,55,10,0,2,1} ;
+//        new ArraySort().sortArray(myArray);
     }
     static void print(){
         System.out.println("Hello World");
--- src/main/java/com/thanu/came/challange3/DataHandler.java
+++ src/main/java/com/thanu/came/challange3/DataHandler.java
@@ -1,7 +1,22 @@
 package com.thanu.came.challange3;
 
-import java.io.Serializable;
+import com.thanu.came.challange3.dto.Student;
+
+import java.util.Scanner;
 
 public class DataHandler {
+
+    public void dataAdding(){
+        var scanner = new Scanner(System.in);
+        var student = new Student();
+
+        System.out.println("Enter Student ID: ");
+        var studentId = scanner.nextLine();
+        student.setStudentId(studentId);
+
+        System.out.println("Enter Student Name: ");
+        var studentName = scanner.nextLine();
+        student.setStudentName(studentName);
+    }
 
 }
