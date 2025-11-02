2a466062ad873ac166c509022c1e23341b376786
ThanuraSilva <thanurasilva1989@gmail.com>
2025-11-02 11:48:34 +0530

    Add license and Code base

---
+++ src/main/java/com/thanu/came/Demo.java
@@ -0,0 +1,10 @@
+package com.thanu.came;
+
+public class Demo {
+    public static void main(String[] args) {
+        print();
+    }
+    static void print(){
+        System.out.println("Hello World");
+    }
+}
