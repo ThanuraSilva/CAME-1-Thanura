375dc733d64dfb75c446eb7cdf52ae14d3f1a714
ThanuraSilva <thanurasilva1989@gmail.com>
2025-11-02 21:51:25 +0530

    Correct the DTO's structure and fix the mistake

--- src/main/java/com/thanu/came/challange3/DataHandler.java
+++ src/main/java/com/thanu/came/challange3/DataHandler.java
@@ -2,31 +2,6 @@ package com.thanu.came.challange3;
 
 import java.io.Serializable;
 
-public class DataHandler implements Serializable {
-    private String studentId;
-    private String studentName;
+public class DataHandler {
 
-    public DataHandler() {
-    }
-
-    public DataHandler(String studentId, String studentName) {
-        this.studentId = studentId;
-        this.studentName = studentName;
-    }
-
-    public String getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getStudentName() {
-        return studentName;
-    }
-
-    public void setStudentName(String studentName) {
-        this.studentName = studentName;
-    }
 }
--- src/main/java/com/thanu/came/challange3/dto/Student.java
+++ src/main/java/com/thanu/came/challange3/dto/Student.java
@@ -1,5 +1,32 @@
 package com.thanu.came.challange3.dto;
 
-public class Student {
+import java.io.Serializable;
 
+public class Student  implements Serializable {
+    private String studentId;
+    private String studentName;
+
+    public Student() {
+    }
+
+    public Student(String studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
 }
